0080b76aef673b631338d1f92e8b32db70824816
Roberto Marban <robertomarban@gmail.com>
2023-04-13 20:08:16 -0300

    codigo funcionando sin variables de entorno y feo

--- sendWA.js
+++ sendWA.js
@@ -1,33 +1,74 @@
-var botId = '105233795800723';
-var phoneNbr = '3213213211';
-var bearerToken = 'EAATZA9dPTFDoBAHzZAaM5FaDspyTHy66co2CQ1ipaJuVhzBHaLQsucZAI1PfnYgdIt3Q7SpbkWywPnE0A2Dew3mXSNwfghuE1sROgBkso9hU0iQbQbgubRGt1IoVdEujQIBdhaE0q2KkYCFctEXM5fcGdWQhfVi7uSXAmP3ryTr2LiwGiQLOiWwf611hnKHb3yUyOH21AZDZD';
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
 
-var url = 'https://graph.facebook.com/v15.0/' + botId + '/messages';
-var data = {
-  messaging_product: 'whatsapp',
-  to: phoneNbr,
-  type: 'template',
-  template: {
-    name:'hello_world',
-    language:{ code: 'en_US' }
+// app.use(cors())
+import fetch from "node-fetch";
+
+//['542494671799','541162920404','541130895432','542214817486',]
+const telefonos = ['542494671799','541162920404','541130895432','542214817486'];
+
+for (let i = 0; i < telefonos.length; i++) {
+  console.log(telefonos[i]);
+  var botId = "115810888147278";
+  var phoneNbr = telefonos[i];
+  var bearerToken =
+    "EAADZAWaZCWBNIBAGfyxZC6Mu557Yxj0IyinjH3aWVRqIZAaxLE7xZCATZCASn8toqrDELwLVUF7qvuGEFaQbF4n3Y1m4bpVaQuTvbMK0uuQHdscdgftatiCrV5UqnnkSa8yQHMKgTQsHsbdVLtTJSVIHtMXk4OPdTtqshGx3cgZAWN7RCtOtB8G7oOtbsxxeZCheZCWtkvNkudVG6nWH1KZA40arFFIoqrW9cZD";
+  
+  var url = "https://graph.facebook.com/v16.0/" + botId + "/messages";
+  var data = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: phoneNbr,
+    type: "template",
+    template: {
+      name: "lalo_sumate",
+      language: {
+        code: "es_AR",
+      },
+      components: [
+        {
+          type: "header",
+          parameters: [
+            {
+              type: "image",
+              image: {
+                link: "https://www.el1digital.com.ar/wp-content/uploads/2022/06/lalo-lalo.jpg",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+  
+  const postReq = {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + bearerToken,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    json: true,
+  };
+  
+  async function enviarMensaje() {
+    try {
+      const response = await fetch(url, postReq);
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
   }
-};
+  
+  console.log(await enviarMensaje());
 
-var postReq = {
-  method: 'POST',
-  headers: {
-    'Authorization': 'Bearer ' + bearerToken,
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data),
-  json: true
-};
 
-fetch(url, postReq)
-  .then(data => {
-    return data.json()
-  })
-  .then(res => {
-    console.log(res)
-  })
-  .catch(error => console.log(error));
+
+}
